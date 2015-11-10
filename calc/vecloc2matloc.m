c0081339202b167e3ecdbc390a5c7b0f0623997b
sciencefans <sciencefans@foxmail.com>
2015-11-11 02:05:24 +0800

    Bug fix

--- calc/vecloc2matloc.m
+++ calc/vecloc2matloc.m
@@ -1,13 +1,16 @@
 function matloc = vecloc2matloc( vecloc, dimsize )
+    if size(vecloc, 2) > 1
+        vecloc = vecloc';
+    end
     matdim = numel(dimsize);
     carrysum(1) = dimsize(1);
     for i = 2 : matdim
         carrysum(i) = carrysum(i-1) * dimsize(i);
     end
     for i = matdim : -1 : 2
-        matloc(i) = floor((vecloc-1) / carrysum(i-1)) + 1;
-        vecloc = mod(vecloc, carrysum(i-1));
+        matloc(:,i) = floor((vecloc-1) ./ carrysum(i-1)) + 1;
+        vecloc = mod(vecloc-1, carrysum(i-1)) + 1;
     end
-    matloc(1) = vecloc;
+    matloc(:,1) = vecloc;
 end
 
